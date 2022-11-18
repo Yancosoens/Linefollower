b18f0e902f7ea87abf679c06a372460e5fb027ae
Yancosoens <115177507+Yancosoens@users.noreply.github.com>
2022-11-18 13:38:59 +0100

    Add files via upload

---
+++ code/proof of concepts/interrupt/DigitalInputPullup/DigitalInputPullup.ino
@@ -0,0 +1,26 @@
+const byte ledPin = 13;
+const byte interruptPin = 2;
+volatile byte state = LOW;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, CHANGE);
+}
+
+void loop() {
+  digitalWrite(ledPin, state);
+}
+
+void blink() {
+{
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  // If interrupts come faster than 200ms, assume it's a bounce and ignore
+  if (interrupt_time - last_interrupt_time > 200) 
+  {
+    state = !state;
+  }
+  last_interrupt_time = interrupt_time;
+}
+}
